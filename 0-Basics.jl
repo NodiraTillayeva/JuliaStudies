3f2335f4014edcfc22ae094dec5edcb30a7f94ff
Nodira Tillayeva (Nara) <ntillayeva@gmail.com>
2023-06-26 12:54:53 +0500

    Add files via upload

---
+++ 0-Basics.jl
@@ -0,0 +1,101 @@
+### A Pluto.jl notebook ###
+# v0.19.26
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 85fb725e-10ce-11ee-3985-092a666018d7
+0.1+0.2 == 0.3
+
+# ╔═╡ 6c00ce97-ebba-460d-8960-eac993df07b9
+typeof(3.14)
+
+# ╔═╡ abde733d-43c0-4179-b5be-d6f72f78590b
+bitstring(0)
+
+# ╔═╡ 5def2375-cc3f-4da0-af19-c72fee5665f7
+bitstring(0.1)
+
+# ╔═╡ c30a6dd2-b579-48ed-a88a-23b9afbab21b
+sqrt(4)
+
+# ╔═╡ 3838dd9c-e687-41bc-b160-88af61a3c539
+cbrt(8)
+
+# ╔═╡ 9e6599e0-5518-48c2-948c-b0567b4a2168
+iseven(2)
+
+# ╔═╡ 6f08b0a2-5746-4d67-85e2-cbb276fdb751
+isodd(3)
+
+# ╔═╡ d17904b0-c2e9-41f5-97f6-0e534fd7a9c9
+rem(5,2)
+
+# ╔═╡ 277cce21-fd00-4054-94c8-79ba333ab913
+round(3.14159, digits =2)
+
+# ╔═╡ 3301b5e9-0ce3-498f-adb3-b757fc35776d
+a =3
+
+# ╔═╡ 063292db-f9fe-47ab-bd9e-40644ef79e5d
+b=3.0
+
+# ╔═╡ 12e6e7b8-d980-448d-bdb4-659aabfbe718
+a==b
+
+# ╔═╡ 4cb04205-4907-4ff0-acdc-b0ab5ae4d6d7
+a===b
+
+# ╔═╡ ec6fe3e6-05b6-45b9-af4b-4428a644afd3
+∛8
+
+# ╔═╡ efb7cd30-875f-4d27-8737-f6c2cf01263b
+#=asdasdasd
+asd
+asd
+asd
+asd
+asd
+=#
+
+# ╔═╡ 955f8dd7-00cc-4d6b-aec6-e309a1932da6
+snowperson = "⛄"
+
+# ╔═╡ b55dc74a-acc6-49f9-915b-669972c05f9b
+snowperson * snowperson
+
+# ╔═╡ 2063ec93-a4a7-4c96-8165-5dac0306a072
+snowperson^5
+
+# ╔═╡ 081cbf63-42cc-483e-85c5-78a5f3d0abf1
+println("¥100")
+
+# ╔═╡ 35e97783-c873-4d08-8149-5869ac5a5e7e
+println("Hello World")
+
+# ╔═╡ 4caa5b5b-d32b-455a-bde4-aede59baa13b
+
+
+# ╔═╡ Cell order:
+# ╠═85fb725e-10ce-11ee-3985-092a666018d7
+# ╠═6c00ce97-ebba-460d-8960-eac993df07b9
+# ╠═abde733d-43c0-4179-b5be-d6f72f78590b
+# ╠═5def2375-cc3f-4da0-af19-c72fee5665f7
+# ╠═c30a6dd2-b579-48ed-a88a-23b9afbab21b
+# ╠═3838dd9c-e687-41bc-b160-88af61a3c539
+# ╠═9e6599e0-5518-48c2-948c-b0567b4a2168
+# ╠═6f08b0a2-5746-4d67-85e2-cbb276fdb751
+# ╠═d17904b0-c2e9-41f5-97f6-0e534fd7a9c9
+# ╠═277cce21-fd00-4054-94c8-79ba333ab913
+# ╠═3301b5e9-0ce3-498f-adb3-b757fc35776d
+# ╠═063292db-f9fe-47ab-bd9e-40644ef79e5d
+# ╠═12e6e7b8-d980-448d-bdb4-659aabfbe718
+# ╠═4cb04205-4907-4ff0-acdc-b0ab5ae4d6d7
+# ╠═ec6fe3e6-05b6-45b9-af4b-4428a644afd3
+# ╠═efb7cd30-875f-4d27-8737-f6c2cf01263b
+# ╠═955f8dd7-00cc-4d6b-aec6-e309a1932da6
+# ╠═b55dc74a-acc6-49f9-915b-669972c05f9b
+# ╠═2063ec93-a4a7-4c96-8165-5dac0306a072
+# ╠═081cbf63-42cc-483e-85c5-78a5f3d0abf1
+# ╠═35e97783-c873-4d08-8149-5869ac5a5e7e
+# ╠═4caa5b5b-d32b-455a-bde4-aede59baa13b
